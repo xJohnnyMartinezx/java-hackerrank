703f0bcb34df34627251c56b1f98de802d3efa80
Johnny Martinez <mrtnz.jn@gmail.com>
2022-06-21 19:36:37 -0500

    Solved Java problem num 2

--- src/main/java/hacker_rank_practice/data/HackerRank.java
+++ src/main/java/hacker_rank_practice/data/HackerRank.java
@@ -1,4 +1,32 @@
 package hacker_rank_practice.data;
 
+import java.util.Scanner;
+
 public class HackerRank {
+    public static void main(String[] args) {
+
+
+//        TODO:
+//        In this challenge, you must read  integers from stdin and then print them to stdout.
+//                Each integer must be printed on a new line. To make the problem a little easier,
+//        a portion of the code is provided for you in the editor below.
+//
+//                Input Format
+//
+//        There are  lines of input, and each line contains a single integer.
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+//HAVE RUN APP AND TYPE 3 INTS TO TEST AGAINST.
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
 }
