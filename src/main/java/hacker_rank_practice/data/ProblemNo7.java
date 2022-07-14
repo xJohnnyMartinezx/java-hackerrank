76446c2159057e5d8767c140c9fa547b20889047
Johnny Martinez <mrtnz.jn@gmail.com>
2022-07-14 16:35:58 -0500

    completed data types excercise on hacker rank.

--- src/main/java/hacker_rank_practice/data/ProblemNo7.java
+++ src/main/java/hacker_rank_practice/data/ProblemNo7.java
@@ -3,17 +3,17 @@ package hacker_rank_practice.data;
 import java.util.Scanner;
 
 public class ProblemNo7 {
-    public static void main(String []argh){
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
 
-            for(int x = 0; x < n; x++){
-                a+=b;
-                if(x > 0){
+            for (int x = 0; x < n; x++) {
+                a += b;
+                if (x > 0) {
                     System.out.print(" ");
                 }
                 System.out.print(a);
