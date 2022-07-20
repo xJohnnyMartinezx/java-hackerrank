67a374b47ef4c871e0ecf008171ee4995da29c41
Johnny Martinez <mrtnz.jn@gmail.com>
2022-07-20 10:46:32 -0500

    Solved HR Java Day of the week coding challenge.

---
+++ src/main/java/hacker_rank_practice/data/JavaDayOfTheWeek.java
@@ -0,0 +1,65 @@
+package hacker_rank_practice.data;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class JavaDayOfTheWeek {
+
+
+    class Result {
+
+        /*
+         * Complete the 'findDay' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. INTEGER month
+         *  2. INTEGER day
+         *  3. INTEGER year
+         */
+
+        public static String findDay(int month, int day, int year) {
+
+            java.time.LocalDate ld = java.time.LocalDate.of(year, month, day);
+            return ld.getDayOfWeek().toString();
+
+//            ****** USE TO STRING AT END TO CONVERT DAY OF WEEK INTO A STRING.
+
+        }
+
+    }
+
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            int month = Integer.parseInt(firstMultipleInput[0]);
+
+            int day = Integer.parseInt(firstMultipleInput[1]);
+
+            int year = Integer.parseInt(firstMultipleInput[2]);
+
+            String res = Result.findDay(month, day, year);
+
+            bufferedWriter.write(res);
+            bufferedWriter.newLine();
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
+    }
+
+}
