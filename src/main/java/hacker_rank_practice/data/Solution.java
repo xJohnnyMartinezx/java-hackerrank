e9e12d199a5f171473dfcdc9daeaf4f8714a6698
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-04 10:42:01 -0700

    Completed HR Inheritance I coding challenge and created Solution class.

---
+++ src/main/java/hacker_rank_practice/data/Solution.java
@@ -0,0 +1,13 @@
+package hacker_rank_practice.data;
+
+public class Solution {
+
+        public static void main(String args[]){
+
+            Bird bird = new Bird();
+            bird.walk();
+            bird.fly();
+            bird.sing();
+
+        }
+}
