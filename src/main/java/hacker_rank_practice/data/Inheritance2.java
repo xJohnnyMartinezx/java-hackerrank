506352e274223edb341a0a32625213d54040ea5b
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-04 11:19:43 -0700

    Completed HR Inheritance II coding challenge

---
+++ src/main/java/hacker_rank_practice/data/Inheritance2.java
@@ -0,0 +1,24 @@
+package hacker_rank_practice.data;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+// Write the following code in your editor below:
+//
+// 1. A class named Arithmetic with a method named add that takes 2 integers as parameters and returns an integer denoting their sum.
+// 2. A class named Adder that inherits from a superclass named Arithmetic.
+//Your classes should not be public.
+
+
+//Write your code here
+class Arithmetic{  /*THE CLASS CREATED */
+    int add(int z, int b){ /* THE ADD METHOD CREATED...HAS TO HAVE A RETURN TYPE...(IN THIS CASE int) "void" IF NO RETURN TYPE */
+        return z + b;
+    }
+}
+
+class Adder extends Arithmetic{ /* ADDER CLASS CREATED THAT INHERITS FROM THE ARITHMETIC CLASS (extends) */
+
+}
