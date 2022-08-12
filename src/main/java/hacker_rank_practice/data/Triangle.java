d4fe18f653dc4314c509a3e00427dc5739361231
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-12 12:19:05 -0700

    ceated Trangle class for codility coding challenge.

---
+++ src/main/java/hacker_rank_practice/data/Triangle.java
@@ -0,0 +1,28 @@
+package hacker_rank_practice.data;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Triangle {
+
+    public int solution(int[] A){
+
+        if (A.length < 3){
+            return 0;
+        }
+
+        Arrays.sort(A);
+
+        for (int i = 0; i <A.length ; i++) {
+            if (A[i] + A[i+1] > A[i+2]){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Triangle().solution(new int[] {10,2,5,1,8,20}));
+    }
+
+}
