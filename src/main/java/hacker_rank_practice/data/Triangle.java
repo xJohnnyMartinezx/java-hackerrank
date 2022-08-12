f804ebf216f4bf5f2781fb6229ce2d2b05036862
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-12 12:22:16 -0700

    Reafactored Traingle and added another test case.

--- src/main/java/hacker_rank_practice/data/Triangle.java
+++ src/main/java/hacker_rank_practice/data/Triangle.java
@@ -13,7 +13,7 @@ public class Triangle {
 
         Arrays.sort(A);
 
-        for (int i = 0; i <A.length ; i++) {
+        for (int i = 0; i <A.length-2; i++) {
             if (A[i] + A[i+1] > A[i+2]){
                 return 1;
             }
@@ -23,6 +23,7 @@ public class Triangle {
 
     public static void main(String[] args) {
         System.out.println(new Triangle().solution(new int[] {10,2,5,1,8,20}));
+        System.out.println(new Triangle().solution(new int[] {10,50,5,1}));
     }
 
 }
