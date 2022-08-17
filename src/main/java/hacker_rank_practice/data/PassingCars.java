b0e06ab4c74423c0dc54b540c84cc3e7da724b49
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-17 16:19:02 -0700

    Created PassingCars class.

---
+++ src/main/java/hacker_rank_practice/data/PassingCars.java
@@ -0,0 +1,13 @@
+package hacker_rank_practice.data;
+
+public class PassingCars {
+
+    public int solution(int[] A){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PassingCars().solution(new int[] {1}));
+    }
+
+}
