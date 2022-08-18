91baa94c9c5424c2b8014830e47f01085c3292f9
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-18 15:38:46 -0700

    Worked out possible solution to MinAbsSumOfTwo.

--- src/main/java/hacker_rank_practice/data/MinAbsSumOfTwo.java
+++ src/main/java/hacker_rank_practice/data/MinAbsSumOfTwo.java
@@ -7,5 +7,23 @@ public class MinAbsSumOfTwo {
 
     public int solution(int[] A){
         Arrays.sort(A);
+
+        int startIndex = 0;
+        int endIndex = A.length -1;
+        int minAbsSum = Integer.MAX_VALUE;
+
+        while (startIndex <= endIndex){
+            int absSum = Math.abs(A[startIndex] + A[endIndex]);
+
+            if (absSum < minAbsSum){
+                minAbsSum = absSum;
+            }
+            if (Math.abs(A[startIndex]) > Math.abs(A[endIndex])){
+                startIndex++;
+            }else {
+                endIndex--;
+            }
+        }
+        return minAbsSum;
     }
 }
