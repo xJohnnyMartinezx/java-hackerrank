057c11b756262c856ff3e9d80f3aa4f521cbdd47
Johnny Martinez <mrtnz.jn@gmail.com>
2022-08-18 15:29:24 -0700

    Created MinAbsSumOfTwo class.

---
+++ src/main/java/hacker_rank_practice/data/MinAbsSumOfTwo.java
@@ -0,0 +1,11 @@
+package hacker_rank_practice.data;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MinAbsSumOfTwo {
+
+    public int solution(int[] A){
+        Arrays.sort(A);
+    }
+}
